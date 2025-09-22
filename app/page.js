61145660c373d77fc4038beef2d9582cf7d43cf2
sakshamwithweb <websaksham39@gmail.com>
2025-09-22 11:54:59 +0530

    wrote about me in bottom

--- app/page.js
+++ app/page.js
@@ -5,8 +5,17 @@ export default function Home() {
 
   return (
     <div className="flex flex-col">
-      <Navbar/>
-      <ProgressBar/>
+      <Navbar />
+      <div className="min-h-[90vh]">
+        <ProgressBar />
+      </div>
+      <div className="w-full border"></div>
+      <div className="py-12 text-center">
+        <p className="max-w-[80%] mx-auto">
+          Hey its me Saksham (github.com/sakshamwithweb), I got frustrated by procastinating my project and playing minecraft(Eaglercraft) then at night regret :/
+          Hence I wanted to do something new and creative everyday as well as I wanted to build things fast that I don't because of procastinating, hence started 52 week 52 stuff in which every week I will be making something really cool and new things, that will push me to learn, solve problems..
+        </p>
+      </div>
     </div>
   );
 }
