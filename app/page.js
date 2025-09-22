617b19931698a09650d27dba3c676ae6cc021b89
sakshamwithweb <websaksham39@gmail.com>
2025-09-22 15:34:02 +0530

    Offically started the challenge

--- app/page.js
+++ app/page.js
@@ -14,6 +14,8 @@ export default function Home() {
         <p className="max-w-[80%] mx-auto">
           Hey its me Saksham (github.com/sakshamwithweb), I got frustrated by procastinating my project and playing minecraft(Eaglercraft) then at night regret :/
           Hence I wanted to do something new and creative everyday as well as I wanted to build things fast that I don't because of procastinating, hence started 52 week 52 stuff in which every week I will be making something really cool and new things, that will push me to learn, solve problems..
+
+          Start Date: 22 September 2025
         </p>
       </div>
     </div>
